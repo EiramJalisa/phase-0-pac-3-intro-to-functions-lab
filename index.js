5e7fe1f55d01c366a7b3c1eafa1c187192352156
+ <+>
2022-08-22 06:59:13 -0500

    commit tree 2020e8781276ba226baa3f95d58ceea3ff39ccf4

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+shout();
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase);
+}
+console.log();
+
+function logWhisper(string) {
+  console.log(string.toLowerCase);
+}
